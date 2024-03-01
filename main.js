19e2295758c3bcf5c3fa23704d9ef8ee5da60773
hieule <hieu.le@rwth-aachen.de>
2024-03-01 16:15:51 +0100

    first

---
+++ main.js
@@ -0,0 +1,60 @@
+import * as THREE from 'three';
+
+// const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 750, window.innerWidth / window.innerHeight, 0.8, 100000 );
+
+const renderer = new THREE.WebGLRenderer();
+
+
+// renderer.setSize( window.innerWidth, window.innerHeight );
+// document.body.appendChild( renderer.domElement );
+
+// const geometry = new THREE.BoxGeometry( 2, 2, 1 );
+// const material = new THREE.MeshBasicMaterial( { color: 0xf1ffaa } );
+// const cube = new THREE.Mesh( geometry, material );
+// scene.add( cube );
+
+// camera.position.z = 5;
+
+// function animate() {
+// 	requestAnimationFrame( animate );
+
+// 	cube.rotation.x += 0.02;
+// 	cube.rotation.y += 0.02;
+
+// 	renderer.render( scene, camera );
+// }
+
+// animate();
+
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+
+const scene = new THREE.Scene();
+
+const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+const points = [];
+points.push( new THREE.Vector3( - 10, 0, 0 ) );
+points.push( new THREE.Vector3( 0, 10, 0 ) );
+points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+const line = new THREE.Line( geometry, material );
+
+scene.add( line );
+renderer.render( scene, camera );
+
+
+
+
+
+
+
+
+
+
