fd923ea696918780015a10a78c3cbd66c264e9e2
hieule <hieu.le@rwth-aachen.de>
2024-03-01 22:39:39 +0100

    first 2

--- main.js
+++ main.js
@@ -1,53 +1,49 @@
 import * as THREE from 'three';
 
-// const scene = new THREE.Scene();
+// define scene
+const scene = new THREE.Scene();
+// define camera
 const camera = new THREE.PerspectiveCamera( 750, window.innerWidth / window.innerHeight, 0.8, 100000 );
-
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
+// define renderer
 const renderer = new THREE.WebGLRenderer();
-
-
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
-
-// const geometry = new THREE.BoxGeometry( 2, 2, 1 );
-// const material = new THREE.MeshBasicMaterial( { color: 0xf1ffaa } );
-// const cube = new THREE.Mesh( geometry, material );
-// scene.add( cube );
-
-// camera.position.z = 5;
-
-// function animate() {
-// 	requestAnimationFrame( animate );
-
-// 	cube.rotation.x += 0.02;
-// 	cube.rotation.y += 0.02;
-
-// 	renderer.render( scene, camera );
-// }
-
-// animate();
-
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+const geometry = new THREE.BoxGeometry( 2, 2, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xf1ffaa } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
 
-const scene = new THREE.Scene();
+camera.position.z = 5;
 
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+function animate() {
+	requestAnimationFrame( animate );
 
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
+	cube.rotation.x += 0.02;
+	cube.rotation.y += 0.02;
 
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
+	renderer.render( scene, camera );
+}
 
-const line = new THREE.Line( geometry, material );
+animate();
 
-scene.add( line );
-renderer.render( scene, camera );
+
+// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+
+// const points = [];
+// points.push( new THREE.Vector3( - 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, 10, 0 ) );
+// points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+// const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+// const line = new THREE.Line( geometry, material );
+
+// scene.add( line );
+
+// renderer.render( scene, camera );
 
 
 
